9ee58b760c090ea667b80085dbadd4fc2033b102
dst3v3n <hablack10@gmail.com>
2023-10-30 17:03:20 -0500

    third commit

--- database.py
+++ database.py
@@ -11,41 +11,52 @@ database = mysql.connector.connect (
 cursor = database.cursor ()
 
 class usuarios:
-    def __init__ (self , Email:str , Password:str):
+    def __init__ (self):
         self.__nombre:str = None
         self.__apellido:str = None
-        self.__email:str = Email
-        self.__password:str = Password
+        self.__email:str = None
+        self.__password:str = None
     
-    def crear_usuarios (self , nombre:str , apellido:str):
-        new_password = encriptar(self.__password)[1]
-        cursor.execute (f'CALL crear_usuario ("{nombre}" , "{apellido}" , "{self.__email}" , "{new_password}")')
+    def crear_usuarios (self , nombre:str , apellido:str , email:str , password:str):
+        new_password = encriptar(password)[1]
+        cursor.execute (f'CALL crear_usuario ("{nombre}" , "{apellido}" , "{email}" , "{new_password}")')
         database.commit ()
         return True
     
     def verificar_usuario (self , correo:str , contraseña:str):
         cursor.execute (f"SELECT * FROM verificacion WHERE email = '{correo}'")
         email , passwd =  cursor.fetchone ()
-        passw = encriptar(contraseña)[1][:-13]  
+        passw = encriptar(contraseña)[1]
         return email == correo and passwd == passw
 
-def eliminar_usuario (correo):
-    cursor.execute (f"CALL delete_usuarios ('{correo}')")
-    database.commit()
-    return True
+    def eliminar_usuario (self , correo):
+        cursor.execute (f"CALL delete_usuarios ('{correo}')")
+        database.commit()
+        return True
 
-def actualizar_nombre (nombre , email):
-    cursor.execute (f'CALL update_nombre ("{nombre}" , "{email}")')
-    database.commit()
-    return True
+    def actualizar_nombre (self , nombre , email):
+        cursor.execute (f'CALL update_nombre ("{nombre}" , "{email}")')
+        database.commit()
+        return True
 
-def actualizar_apellido (apellido , email):
-    cursor.execute (f'CALL update_apellido ("{apellido}" , "{email}")')
-    database.commit()
-    return True
+    def actualizar_apellido (self , apellido , email):
+        cursor.execute (f'CALL update_apellido ("{apellido}" , "{email}")')
+        database.commit()
+        return True
 
-def actualizar_passwd (passwd , email):
-    new_password = encriptar(passwd)[1]
-    cursor.execute (f'CALL update_passwd ("{new_password}" , "{email}")')
-    database.commit()
-    return True
+    def actualizar_passwd (self , passwd , email):
+        new_password = encriptar(passwd)[1]
+        cursor.execute (f'CALL update_passwd ("{new_password}" , "{email}")')
+        database.commit()
+        return True
+    
+    def dato (self , row):
+        return {"nombre" : row[0],
+                "apellido" : row[1],
+                "email" : row[2],
+                "password" : row[3]}
+    
+    def datos (self):
+        cursor.execute("SELECT * FROM leer_usuarios")
+        x = cursor.fetchall ()
+        return [self.dato(row) for row in x]
--- front.py
+++ front.py
@@ -15,15 +15,15 @@ async def root ():
 @app.post ("/data")
 async def usuario (nombre:str = Form() , apellido:str = Form() , email:str = Form() , password:str = Form() , re_password:str = Form()):
     if password == re_password:
-        new = usuarios(email , password)
-        new.crear_usuarios(nombre , apellido)
+        new = usuarios()
+        new.crear_usuarios(nombre , apellido , email , password)
         return {"exito" : "REGISTRO EXITOSO"}
     else:
         return {"404" : "not found"}
 
 @app.post ("/inicio")
 async def user (username:str = Form() , password:str = Form()):
-    new = usuarios (username , password)
+    new = usuarios ()
     if new.verificar_usuario(username , password) == True:
         return {"exito" : "Inicio de sesion exitoso"}
     else:
@@ -31,20 +31,29 @@ async def user (username:str = Form() , password:str = Form()):
     
 @app.delete ("/eliminar_usuario/")
 async def eliminacion (email:str):
-    if eliminar_usuario (email) == True:
+    new = usuarios () 
+    if new.eliminar_usuario (email) == True:
         return {"exitoso" : "Eliminacion exitosa"}
     
 @app.put ("/actualizar_dato/nombre/")
 async def actualizar_nombres (nombre , email):
-    if actualizar_nombre (nombre , email) == True:
+    new = usuarios () 
+    if new.actualizar_nombre (nombre , email) == True:
         return {"exitoso" : "actualizacion exitosa"}
     
 @app.put ("/actualizar_dato/apellido/")
 async def actualizar_apellidos (apellido , email):
-    if actualizar_apellido (apellido , email) == True:
+    new = usuarios () 
+    if new.actualizar_apellido (apellido , email) == True:
         return {"exitoso" : "actualizacion exitosa"}
     
 @app.put ("/actualizar_dato/password/")
 async def actualizar_passwords (password , email):
-    if actualizar_passwd (password , email) == True:
+    new = usuarios () 
+    if new.actualizar_passwd (password , email) == True:
         return {"exitoso" : "actualizacion exitosa"}
+    
+@app.get ("/visualizar/datos")
+async def visualizar_todos ():
+    new = usuarios ()
+    return new.datos ()
